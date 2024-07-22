7223db039b36b394dc5401771588db788566f2dd
xxo <huyle210504@gmail.com>
2024-07-22 22:58:11 +0700

    m

--- draft/draft.py
+++ draft/draft.py
@@ -54,20 +54,21 @@ from xgboost import XGBClassifier
 import re
 from sklearn.base import BaseEstimator, TransformerMixin
 
-# class Onehot_Scaling_PCA for SFS
+# class FS Base
 class FS_BaseUserDefinedTransformer(BaseEstimator, TransformerMixin):
-    ##
-    def __init__(self):
+    ## 
+    def __init__(self, 
+                 ohe=None, other_encoders=None,
+                 scaling: list=[], factor_analysis: list=[]) -> None:
         ###
-        scaler, pca = [('normalize', StandardScaler())], [('PCA', PCA(n_components=0.8))]
-        steps = scaler + pca
-        self.num_pro = Pipeline(steps)
+        self.scaling, self.factor_analysis = scaling, factor_analysis
+        ### 
+        self.ohe, self.other_encoders = ohe, other_encoders
 
-        ###
-        self.cat_pro = OneHotEncoder(drop='first', sparse_output=False)
 
     ##
     def _check_ndim(self, X: np.ndarray) -> tuple[np.ndarray, int]:
+        ###
         if X.ndim == 2:
             X_ = X
             num_iters = X.shape[1]
@@ -76,14 +77,16 @@ class FS_BaseUserDefinedTransformer(BaseEstimator, TransformerMixin):
             num_iters = 1
 
         return X_, num_iters
-
-    ## 
+    
+    ##
     def _category_detection(self, X: np.ndarray) -> tuple[list, list, np.ndarray]:
         ###
         num_idxes, cat_idxes = [], []
 
         ### check dimension
         X_, num_iters = self._check_ndim(X=X)
+
+        ###
         for i in range(num_iters):
             try:
                 X_[:1, i].astype(float)
@@ -92,31 +95,34 @@ class FS_BaseUserDefinedTransformer(BaseEstimator, TransformerMixin):
                 cat_idxes.append(i)
         
         return num_idxes, cat_idxes, X_
+    
+    ##
+    def _get_transformers(self, cat_idxes: list=[], num_idxes: list=[]) -> list:
+        if len(num_idxes) == 0:
+            transformers = self.cat_pro
+        elif len(cat_idxes) == 0:
+            transformers = self.num_pro
+        else:
+            transformers = self.num_pro + self.cat_pro
 
+        return transformers 
+    
+    ## 
     def fit(self, X: np.ndarray, y=None):
         ###
-        self.num_idxes, self.cat_idxes, X_ = self._category_detection(X)
+        transformers = self._get_transformers(self.cat_idxes, self.num_idxes)
+        self.ct = ColumnTransformer(transformers, remainder='passthrough')
+        self.ct.fit(self.X_fit_)
 
-        ### 
-        if len(self.num_idxes) == 0:
-            transformers = [('cat_pro', self.cat_pro, self.cat_idxes)]
-        elif len(self.cat_idxes) == 0:
-            transformers = [('num_pro', self.num_pro, self.num_idxes)]
-        else:
-            transformers = [('num_pro', self.num_pro, self.num_idxes), 
-                           ('cat_pro', self.cat_pro, self.cat_idxes)]
-            
-        ###
-        self.ct = ColumnTransformer(transformers=transformers, remainder='passthrough')
-        self.ct.fit(X=X_)
-        
         return self
     
+    ##
     def transform(self, X: np.ndarray, y=None):
         X_, _ = self._check_ndim(X=X)
-        return self.ct.transform(X_)
-    
 
+        return self.ct.transform(X=X_)
+
+# Onehot_Scaling_Pca
 class SFS_OSP(FS_BaseUserDefinedTransformer):
     def fit(self, X: np.ndarray, y=None):
         ###
@@ -124,21 +130,14 @@ class SFS_OSP(FS_BaseUserDefinedTransformer):
 
         ###
         steps = self.scaling + self.factor_analysis
-        self.num_pro = [('num_pro', Pipeline(steps), num_idxes)]
+        self.num_pro = [('num_pro', Pipeline(steps), self.num_idxes)]
 
-        self.cat_pro = [('cat_pro', self.ohe, cat_idxes)]
+        self.cat_pro = [('cat_pro', self.ohe, self.cat_idxes)]
 
         ###
-        transformers = self._get_transformers(cat_idxes, num_idxes)
-        self.ct = ColumnTransformer(transformers, remainder='passthrough')
-        self.ct.fit(X=self.X_fit_)
-
-        return self
-
-    def transform(self, X: np.ndarray, y=None):
-        X_, _ = self._check_ndim(X=X)
+        super().fit(X=X)
         
-        return self.ct.transform(X_)
+        return self
 
 
 
