060a2b1f0f6ea55aa7dd1ccc07babca5fe2e2a00
xxo <huyle210504@gmail.com>
2024-07-28 22:58:34 +0700

    m

---
+++ prefect/orchestrate.py
@@ -0,0 +1,14 @@
+# prefect
+from prefect import flow, task
+
+@task
+def testing_task():
+    return 'hello world'
+
+@flow
+def testing_flow():
+    message = testing_task()
+    print(message)
+
+if __name__ == '__main__':
+    testing_flow()
