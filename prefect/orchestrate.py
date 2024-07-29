dd14a6c3ea38bcde5a9a4fb8c43a9c06c54e17a2
xxo <huyle210504@gmail.com>
2024-07-29 15:23:43 +0700

    m

--- prefect/orchestrate.py
+++ prefect/orchestrate.py
@@ -1,15 +1,24 @@
+# data structures
+import numpy as np
+import pandas as pd
 # prefect
 from prefect import flow, task
+# user-defined moudles
+import os
+cwd = os.getcwd()
+os.chdir('..')
+from  usr_modules.prefect_usr_defined import format_adjustment
+os.chdir(cwd)
 
-# 
-@task
-def testing_task():
-    return 'hello world'
+# load dataset
+@task(name='Load Dataset')
+@format_adjustment
+def load_dataset(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path)
+    return df
 
-@flow
-def testing_flow():
-    message = testing_task()
-    print(message)
-
-if __name__ == '__main__':
-    testing_flow()
+@task(name='Data Wrangling')
+def data_wrangling(
+    df_train: pd.DataFrame, df_test: pd.DataFrame
+) -> tuple[pd.DataFrame, pd.DataFrame]:
+    return 
--- usr_modules/prefect_usr_defined.py
+++ usr_modules/prefect_usr_defined.py
@@ -1,5 +1,31 @@
 # data structures
 import numpy as np
 import pandas as pd
-# prefect
-from prefect import flow, task
+# others
+import functools
+
+#
+def format_adjustment(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kargs) -> pd.DataFrame:
+        df: pd.DataFrame = func(*args, **kargs)
+        ##
+        df_format_adjusted.columns = [name.strip() for name in df.columns.tolist()]
+        cat_cols = df.select_dtyeps('object')\
+            .columns.tolist()
+        ##
+        df_format_adjusted = df.copy()
+        ##
+        df_format_adjusted[cat_cols] = df_format_adjusted[cat_cols]\
+            .map(lambda x: x.strip())
+        ##
+        mask = (df_format_adjusted[cat_cols] == '')\
+            .any(axis=0)
+        null_names = mask[mask == True]\
+            .index.tolist()
+        df_format_adjusted[null_names] = df_format_adjusted[null_names]\
+            .map(lambda x: np.nan if x == '' else x)
+
+        return df_format_adjusted
+    
+    return wrapper
