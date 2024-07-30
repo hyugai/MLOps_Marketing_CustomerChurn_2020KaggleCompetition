3b4e11df2d33b86809f4fa61f5916cf8b6270e6e
xxo <huyle210504@gmail.com>
2024-07-30 10:44:48 +0700

    m

--- prefect/orchestrate.py
+++ prefect/orchestrate.py
@@ -7,21 +7,27 @@ from prefect import flow, task
 from usr_modules.prefect_usr_defined import *
 
 # load dataset
-@task(name='Load Dataset')
+@task(name='Load Dataset', log_prints=False)
 @format_adjustment
 def load_dataset(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     return df
 
-@task(name='Data Wrangling')
-def data_wrangling(
-    df_train: pd.DataFrame, df_test: pd.DataFrame
-) -> tuple[pd.DataFrame, pd.DataFrame]:
-    return 
+@task(name='Data Wrangling', log_prints=False)
+@handling_single_value_columns
+@handling_duplications
+@detect_missing_values
+def data_wrangling(df: pd.DataFrame) -> pd.DataFrame:
+    return df
+
+@task(name='Compatible validation')
+def compatible_validation(train: pd.DataFrame, test: pd.DataFrame):
+    return train, test
 
 @flow
 def main_flow() -> None:
     df_train = load_dataset(path='../dataset/raw/train.csv')
+    df_train = data_wrangling(df_train)
 
 if __name__ == '__main__':
     main_flow()
--- prefect/usr_modules/prefect_usr_defined.py
+++ prefect/usr_modules/prefect_usr_defined.py
@@ -14,18 +14,71 @@ def format_adjustment(func):
         cat_cols = df.select_dtypes('object')\
             .columns.tolist()
         ## cloning
-        df_format_adjusted = df.copy()
+        df_cleaned = df.copy()
         ## stripping 
-        df_format_adjusted[cat_cols] = df_format_adjusted[cat_cols]\
+        df_cleaned[cat_cols] = df_cleaned[cat_cols]\
             .map(lambda x: x.strip())
         ## null strings
-        mask = (df_format_adjusted[cat_cols] == '')\
+        mask = (df_cleaned[cat_cols] == '')\
             .any(axis=0)
         null_names = mask[mask == True]\
             .index.tolist()
-        df_format_adjusted[null_names] = df_format_adjusted[null_names]\
+        df_cleaned[null_names] = df_cleaned[null_names]\
             .map(lambda x: np.nan if x == '' else x)
 
-        return df_format_adjusted
+        return df_cleaned
     
     return wrapper
+
+#
+def detect_missing_values(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kargs) -> pd.DataFrame:
+        df: pd.DataFrame = func(*args, **kargs)
+        ## counts
+        mask = df.isnull()
+        counts = mask.sum(axis=0)
+        print(f'Total missing values per column: \n{counts}')
+        
+        return df
+
+    return wrapper
+
+#
+def handling_duplications(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kargs) -> pd.DataFrame:
+        df: pd.DataFrame = func(*args, **kargs)
+        ##counts
+        mask = df.duplicated()
+        counts = mask.sum()
+        print(f'Total duplications: {counts}')
+        ##
+        df.drop_duplicates(inplace=True)
+
+        return df
+    
+    return wrapper
+
+# 
+def handling_single_value_columns(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kargs) -> pd.DataFrame:
+        df: pd.DataFrame = func(*args, **kargs)
+        ## detection
+        nuniques = df.nunique()
+        single_value_names = nuniques[nuniques == 1].index.tolist()
+        print(f'Single value columns\'s name: {single_value_names}')
+        ##
+        try:
+            df.drop(
+                labels=single_value_names, axis=1, 
+                inplace=True
+            )
+        except:
+            print('Please check column\'s names again')
+
+        return df
+    
+    return wrapper
+    
