d002cab036f422e8964723e1f734778e83bb3c51
xxo <huyle210504@gmail.com>
2024-07-30 15:02:28 +0700

    m

--- prefect/orchestrate.py
+++ prefect/orchestrate.py
@@ -8,26 +8,24 @@ from usr_modules.prefect_usr_defined import *
 
 # load dataset
 @task(name='Load Dataset', log_prints=False)
-@format_adjustment
+@adjust_format
 def load_dataset(path: str) -> pd.DataFrame:
     df = pd.read_csv(path)
     return df
 
 @task(name='Data Wrangling', log_prints=False)
-@handling_single_value_columns
-@handling_duplications
+@split_dataset
+@handle_single_value_columns
+@handle_duplications
 @detect_missing_values
 def data_wrangling(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
-@task(name='Compatible validation')
-def compatible_validation(train: pd.DataFrame, test: pd.DataFrame):
-    return train, test
 
-@flow
+@flow(log_prints=False)
 def main_flow() -> None:
-    df_train = load_dataset(path='../dataset/raw/train.csv')
-    df_train = data_wrangling(df_train)
+    df = load_dataset(path='../dataset/raw/train.csv')
+    df_train, df_test = data_wrangling(df)
 
 if __name__ == '__main__':
     main_flow()
--- prefect/usr_modules/prefect_usr_defined.py
+++ prefect/usr_modules/prefect_usr_defined.py
@@ -1,11 +1,13 @@
 # data structures
 import numpy as np
 import pandas as pd
+# model selection
+from sklearn.model_selection import train_test_split
 # others
 import functools
 
 #
-def format_adjustment(func):
+def adjust_format(func):
     @functools.wraps(func)
     def wrapper(*args, **kargs) -> pd.DataFrame:
         df: pd.DataFrame = func(*args, **kargs)
@@ -45,7 +47,7 @@ def detect_missing_values(func):
     return wrapper
 
 #
-def handling_duplications(func):
+def handle_duplications(func):
     @functools.wraps(func)
     def wrapper(*args, **kargs) -> pd.DataFrame:
         df: pd.DataFrame = func(*args, **kargs)
@@ -61,14 +63,14 @@ def handling_duplications(func):
     return wrapper
 
 # 
-def handling_single_value_columns(func):
+def handle_single_value_columns(func):
     @functools.wraps(func)
     def wrapper(*args, **kargs) -> pd.DataFrame:
         df: pd.DataFrame = func(*args, **kargs)
         ## detection
         nuniques = df.nunique()
         single_value_names = nuniques[nuniques == 1].index.tolist()
-        print(f'Single value columns\'s name: {single_value_names}')
+        print(f'Single-value columns\'s name: {single_value_names}')
         ##
         try:
             df.drop(
@@ -81,5 +83,55 @@ def handling_single_value_columns(func):
         return df
     
     return wrapper
-    
+
 #
+def split_dataset(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kargs) -> tuple[pd.DataFrame, pd.DataFrame]:
+        df: pd.DataFrame = func(*args, **kargs)
+        ##
+        column_names = df.columns
+        X, y = df[column_names.drop('churn').tolist()].values, df['churn'].values
+        ##
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, 
+            test_size=0.3, random_state=7, 
+            stratify=y 
+        )
+        df_train = pd.DataFrame(
+            data= np.hstack(tup=[X_train, y_train.reshape(-1, 1)]), 
+            columns=column_names
+        )
+        df_test = pd.DataFrame(
+            data=np.hstack(tup=[X_test, y_test.reshape(-1, 1)]), 
+            columns=column_names
+        )
+
+        return df_train, df_test
+    
+    return wrapper
+
+#
+def validate_columns_compatible(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kargs) -> None:
+        df_train, df_test = func(*args, **kargs)
+        ## detections
+        column_names = dict(
+            train=np.sort(df_train.columns.to_numpy()), 
+            test=np.sort(df_test.columns.to_numpy())
+        )
+        try:
+            mask = column_names['train'] != column_names['test']
+            if np.sum(mask) != 0:
+                for train_col_name, test_col_name in zip(column_names['train'][mask], column_names['test'][mask]):
+                    print(f'{train_col_name} is not compatible with {test_col_name}')
+            else:
+                print('Columns compatile: Passed.')
+        except:
+            print(f'Please check the number of columns of each file again.')
+        
+
+        return df_train, df_test
+    
+    return wrapper
