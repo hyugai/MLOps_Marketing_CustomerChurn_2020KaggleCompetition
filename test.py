d2967234c5f1a8f354ad598b9f62e906ddace762
xxo <huyle210504@gmail.com>
2024-08-06 18:48:09 +0700

    m

--- test.py
+++ test.py
@@ -5,7 +5,14 @@ from src.mlflow.mlflow_usr_defined import *
 from src.notebook.features_engineering import *
 
 import joblib
+import pickle
 import dill
+
+class UnpicklerForSFS(pickle.Unpickler):
+    def find_class(self, module_name: str, global_name: str):
+        if module_name == '__main__':
+            module_name = 'test'
+        return super().find_class(module_name, global_name)
 
 @task(log_prints=True)
 def try_loadinng_joblib():
@@ -14,10 +21,24 @@ def try_loadinng_joblib():
         model = dill.load(input)
     return model
 
+@task(log_prints=True)
+def try_customized_unpickler():
+    with open('storage/temp/unpickler.pkl', 'rb') as f:
+        unpickler = UnpicklerForSFS(f)
+        model = unpickler.load()
+
+    return model
+
+@flow(log_prints=True)
+def main_flow_v0():
+    model = try_customized_unpickler()
+
+
 @flow(log_prints=True)
 def main_flow():
     model = try_loadinng_joblib()
     print(model)
 
 if __name__ == '__main__':
-    main_flow.serve('joblib')
+    #main_flow.serve('joblib')
+    main_flow_v0.serve('unpickler')
